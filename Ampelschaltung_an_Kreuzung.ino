f6ec583bd0a2fd76e8ecc54503655135bccb8315
TheMoonlandingV2 <88386049+TheMoonlandingV2@users.noreply.github.com>
2021-12-05 14:23:41 +0100

    Add files via upload

---
+++ Ampelschaltung_an_Kreuzung.ino
@@ -0,0 +1,66 @@
+const int rot1 = 5;
+const int gelb1 = 6;
+const int grun1 = 7;
+
+const int rot2 = 2;
+const int gelb2 = 3;
+const int grun2 = 4;
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(rot1, OUTPUT);
+  pinMode(gelb1, OUTPUT);
+  pinMode(grun1, OUTPUT);
+  pinMode(rot2, OUTPUT);
+  pinMode(gelb2, OUTPUT);
+  pinMode(grun2, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(rot2, HIGH);
+  schaltung1();
+  delay(5000);
+  schaltung2();
+  delay(5000);
+}
+
+
+void schaltung1() {
+  digitalWrite(rot1, HIGH);
+  delay(1000);
+  digitalWrite(gelb1, HIGH);
+  delay(1000);
+  digitalWrite(rot1, LOW);
+  delay(10);
+  digitalWrite(gelb1, LOW);
+  delay(100);
+  digitalWrite(grun1, HIGH);
+  delay(5000);
+  digitalWrite(grun1, LOW);
+  digitalWrite(gelb1, HIGH);
+  delay(1000);
+  digitalWrite(gelb1, LOW);
+  digitalWrite(rot1, HIGH);
+}
+
+void schaltung2(){
+  digitalWrite(rot2, HIGH);
+  delay(1000);
+  digitalWrite(gelb2, HIGH);
+  delay(1000);
+  digitalWrite(rot2, LOW);
+  delay(10);
+  digitalWrite(gelb2, LOW);
+  delay(100);
+  digitalWrite(grun2, HIGH);
+  delay(5000);
+  digitalWrite(grun2, LOW);
+  digitalWrite(gelb2, HIGH);
+  delay(1000);
+  digitalWrite(gelb2, LOW);
+  digitalWrite(rot2, HIGH);
+}
